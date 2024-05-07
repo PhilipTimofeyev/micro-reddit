003fa5719786c37fb61bd311c5e8615b46550e75
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-07 08:29:08 -0700

    Update associations and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_150346) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_150726) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
